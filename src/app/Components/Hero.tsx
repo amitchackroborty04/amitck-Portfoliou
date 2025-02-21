b9d2aa144e3919bfbee0ac9c499239002c2b4bf7
Amit Chackroborty <149465275+amitchackroborty04@users.noreply.github.com>
2025-02-21 22:48:38 +0600

    develop animation

--- src/app/Components/Hero.tsx
+++ src/app/Components/Hero.tsx
@@ -1,8 +1,12 @@
 "use client";
 import Image from "next/image";
 import React, { useEffect, useRef } from "react";
-import Card from "./Card";
+
 import { motion } from "framer-motion";
+import { FlipText } from "@/components/magicui/flip-text";
+import { Particles } from "@/components/magicui/particles";
+import { ShimmerButton } from "@/components/magicui/shimmer-button";
+
 // import reacticon from "@/../public/imge/react.png"
 // import icon from "../../../public/imge/react.png"
 
@@ -17,56 +21,46 @@ const Hero = () => {
     }
   }, []);
 
-  const textVariants = {
-    hidden: { opacity: 0, y: 20 },
-    visible: (i: number) => ({
-      opacity: 1,
-      y: 0,
-      transition: { delay: i * 0.2 },
-    }),
-  };
-  const name = "Amit Chakraborty";
-  const highlight = "Cha";
+ 
+  
+ 
   return (
     <section className="bg-gradient-to-r from-[#04016C]/80 to-[#4A16BD]/80 dark:from-[#1A1A29] dark:to-[#1A1A29]">
       <div className="container flex flex-col lg:flex-row gap-y-10 lg:gap-x-10 items-center pt-[117px] relative">
         {/* Text and buttons container */}
+        <Particles
+        className="absolute inset-0 z-0"
+        quantity={100}
+        ease={10}
+        color={"#7562E0"}
+        refresh
+      />
         <div className="w-full lg:w-[50%] text-center lg:text-left">
           <motion.h1
             initial={{ opacity: 0, y: -50 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8, ease: "easeOut" }} 
-           className="text-[32px] text-[#FFFFFF] font-semibold">
+            transition={{ duration: 0.8, ease: "easeOut" }}
+            className="text-[32px] text-[#FFFFFF] font-semibold"
+          >
             Hello, I’m
           </motion.h1>
-          <h2 className="text-[30px] lg:text-[52px] text-[#FFFFFF] font-semibold mt-2">
-      {name.split("").map((char, index) => (
-        <motion.span
-          key={index}
-          custom={index}
-          variants={textVariants}
-          initial="hidden"
-          animate="visible"
-          className={highlight.includes(char) ? "text-[#4BE5CA] dark:text-[#FFFFFF]" : ""}
-        >
-          {char}
-        </motion.span>
-      ))}
-    </h2>
+          <FlipText className="!text-[25px] lg:!text-[52px] text-[#FFFFFF] font-semibold mt-2">
+            Amit Chakraborty
+          </FlipText>
           <p className="text-[18px] text-[#FFFFFF] font-medium mt-2 w-full   lg:w-[435px]">
             Freelance UI designer, Fullstack developer, & Data Miner. I create
             seamless web experiences for end-users.
           </p>
           <div className="mt-7 flex justify-center lg:justify-start">
-            <button className="text-base text-[#FFFFFF] font-semibold border-[1px] border-[#7562E0] hover:bg-[#7562E0] py-3 px-6 rounded-[12px] hover:scale-95 duration-300">
+            <ShimmerButton className="text-base !text-black  dark:text-[#FFFFFF]  font-semibold border-[1px] border-[#7562E0]  py-3 px-6 rounded-[12px] ">
               About me
-            </button>
-            <button className="text-base text-[#FFFFFF] font-semibold border-[1px] border-[#7562E0] hover:bg-[#7562E0] py-3 px-6 rounded-[12px] hover:scale-95 duration-300 ml-5">
+            </ShimmerButton>
+            <ShimmerButton className="text-base !text-black dark:text-[#FFFFFF] font-semibold border-[1px] border-[#7562E0]  py-3 px-6 rounded-[12px]  ml-5">
               Project
-            </button>
+            </ShimmerButton>
           </div>
           <div className="w-full h-[80px] md:h-[128px] dark:bg-[#181824] bg-[#FFFFFF] absolute bottom-0 left-0 overflow-x-hidden ">
-            <div
+            {/* <div
               ref={marqueeRef}
               className="flex gap-4 min-w-max animate-marquee"
             >
@@ -75,17 +69,18 @@ const Hero = () => {
               <Card title="bootstrap" src="/imge/react.png" />
               <Card title="tailwind" src="/imge/react.png" />
               <Card title="React" src="/imge/react.png" />
-              <Card title="nextjs" src="/imge/react.png"/>
-            </div>
+              <Card title="nextjs" src="/imge/react.png" />
+            </div> */}
           </div>
         </div>
 
         {/* Image container */}
         <motion.div
-         initial={{ opacity: 0, x: 50 }}
-         animate={{ opacity: 1, x: 0 }}
-         transition={{ duration: 0.9, ease: "easeOut" }}
-         className="w-full lg:w-[50%] flex justify-center sm:w-full mt-5 lg:mt-0">
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.9, ease: "easeOut" }}
+          className="w-full lg:w-[50%] flex justify-center sm:w-full mt-5 lg:mt-0"
+        >
           <Image
             src="/imge/hero.png"
             alt="hero"
--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -1,6 +1,7 @@
-'use client'
-import React, { useState } from 'react'
-import { Darkbutton } from './DarkButton'
+"use client";
+import React, { useState } from "react";
+import { Darkbutton } from "./DarkButton";
+import { SparklesText } from "@/components/magicui/sparkles-text";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -10,34 +11,47 @@ const Navbar = () => {
   };
 
   return (
-    <header className='overflow-x-hidden'>
-      <div className='relative '>
-        <div className='container flex justify-between items-center bg-[#FFFFFF] dark:bg-[#181824] py-5 fixed z-50'>
+    <header className="overflow-x-hidden">
+      <div className="relative ">
+        <div className="container flex justify-between items-center bg-[#FFFFFF] dark:bg-[#181824] py-5 fixed z-50">
           {/* Logo and mobile toggle */}
-          <div className='flex items-center justify-between w-full'>
-            <div className='flex items-center'>
-              {/* <Image src={logo} alt="" width={100} height={100} className='w-[80px] h-[80px] object-cover rounded-full bg-transparent' /> */}
-              <h2 className='text-xl lg:text-[28px] text-[#1B2336] dark:text-[#7562E0] font-semibold'>Amit <span className='text-[#2AAA94] dark:text-[#7562E0]'>Chakraborty</span> </h2>
+          <div className="flex items-center justify-between w-full">
+            <div className="flex items-center">
+              <SparklesText
+                className="text-xl lg:text-[28px] text-[#1B2336] dark:text-[#7562E0] font-semibold"
+                text="Amit Chakraborty"
+              />
             </div>
 
             {/* Mobile hamburger button */}
-            <div className="md:hidden">
-              <button 
-                onClick={() => setIsMenuOpen(!isMenuOpen)} 
+            <div className="md:hidden flex items-center gap-4">
+              <button
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
                 className="text-2xl text-black dark:text-white"
               >
-                {isMenuOpen ? 'X' : '☰'}
+                {isMenuOpen ? "X" : "☰"}
               </button>
+              <div>
+                <Darkbutton />
+              </div>
             </div>
           </div>
 
           {/* Desktop Menu */}
-          <nav className='hidden md:flex '>
-            <ul className='flex  md:gap-[40px] lg:gap-[80px] items-center  cursor-pointer '>
-              <li className='text-[18px] dark:text-[#FFFFFF] text-[#1B2336] font-semibold hover:scale-75 duration-300 '>Home</li>
-              <li className='text-[18px] dark:text-[#FFFFFF] text-[#1B2336] font-semibold hover:scale-75 duration-300 text-nowrap'>About me</li>
-              <li className='text-[18px] dark:text-[#FFFFFF] text-[#1B2336] font-semibold hover:scale-75 duration-300'>Project</li>
-              <li className='text-[18px] dark:text-[#FFFFFF] text-[#1B2336] font-semibold hover:scale-75 duration-300'>Contact</li>
+          <nav className="hidden md:flex ">
+            <ul className="flex  md:gap-[40px] lg:gap-[80px] items-center  cursor-pointer ">
+              <li className="text-[18px] dark:text-[#FFFFFF] text-[#1B2336] font-semibold hover:scale-75 duration-300 ">
+                Home
+              </li>
+              <li className="text-[18px] dark:text-[#FFFFFF] text-[#1B2336] font-semibold hover:scale-75 duration-300 text-nowrap">
+                About me
+              </li>
+              <li className="text-[18px] dark:text-[#FFFFFF] text-[#1B2336] font-semibold hover:scale-75 duration-300">
+                Project
+              </li>
+              <li className="text-[18px] dark:text-[#FFFFFF] text-[#1B2336] font-semibold hover:scale-75 duration-300">
+                Contact
+              </li>
               <div>
                 <Darkbutton />
               </div>
@@ -46,31 +60,38 @@ const Navbar = () => {
         </div>
 
         {/* Mobile Slide-out Menu */}
-        <div 
-          className={`md:hidden fixed z-50 top-0 right-0 bg-[#c2c2e4] dark:bg-[#181824] w-[250px] h-full transform ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300`}
+        <div
+          className={`md:hidden fixed z-50 top-0 right-0 bg-[#c2c2e4] dark:bg-[#181824] w-[250px] h-full transform ${
+            isMenuOpen ? "translate-x-0" : "translate-x-full"
+          } transition-transform duration-300`}
         >
           {/* Cross button to close menu */}
           <div className="absolute top-4 right-4">
-            <button 
-              onClick={closeMenu} 
+            <button
+              onClick={closeMenu}
               className="text-2xl text-black dark:text-white"
             >
               X
             </button>
           </div>
-          <ul className='flex flex-col items-center justify-center gap-8 pt-20 cursor-pointer'>
-            <li className='text-[18px] dark:text-[#FFFFFF] text-[#1B2336] font-semibold hover:scale-75 duration-300'>Home</li>
-            <li className='text-[18px] dark:text-[#FFFFFF] text-[#1B2336] font-semibold hover:scale-75 duration-300'>About me</li>
-            <li className='text-[18px] dark:text-[#FFFFFF] text-[#1B2336] font-semibold hover:scale-75 duration-300'>Project</li>
-            <li className='text-[18px] dark:text-[#FFFFFF] text-[#1B2336] font-semibold hover:scale-75 duration-300'>Contact</li>
-            <div>
-              <Darkbutton />
-            </div>
+          <ul className="flex flex-col items-center justify-center gap-8 pt-20 cursor-pointer">
+            <li className="text-[18px] dark:text-[#FFFFFF] text-[#1B2336] font-semibold hover:scale-75 duration-300">
+              Home
+            </li>
+            <li className="text-[18px] dark:text-[#FFFFFF] text-[#1B2336] font-semibold hover:scale-75 duration-300">
+              About me
+            </li>
+            <li className="text-[18px] dark:text-[#FFFFFF] text-[#1B2336] font-semibold hover:scale-75 duration-300">
+              Project
+            </li>
+            <li className="text-[18px] dark:text-[#FFFFFF] text-[#1B2336] font-semibold hover:scale-75 duration-300">
+              Contact
+            </li>
           </ul>
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
 export default Navbar;
